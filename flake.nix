7a774bdb19b86c2f7725cf93553d88b58c671f7b
Corentin Glaus <glaus.corentin@gmail.com>
2026-04-20 23:31:33 -0400

    Init

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          rustc
+          cargo
+          gcc
+          pkg-config
+        ];
+      };
+    };
+}
